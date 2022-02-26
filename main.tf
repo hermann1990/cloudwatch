249d5d468bdf04ffd378586bc35b58eeecc7024b
hermann <hermann042009@hotmail.com>
2022-02-26 02:44:00 -0500

    cw script

--- main.tf
+++ main.tf
@@ -1,24 +1,24 @@
 provider "aws" {
-  region = "ap-southeast-1"
+  region = var.region
 }
 
 locals {
-  userdata = templatefile("userdata.sh", {
+  userdata = templatefile(var.userdata ,  {
     ssm_cloudwatch_config = aws_ssm_parameter.cw_agent.name
   })
 }
 
 resource "aws_instance" "this" {
-  ami                  = "ami-0cbc6aae997c6538a"
+  ami                  = "ami-0ed9277fb7eb570c9"
   instance_type        = "t3.micro"
   iam_instance_profile = aws_iam_instance_profile.this.name
   user_data            = local.userdata
   tags                 = { Name = "EC2-with-cw-agent" }
 }
 
-resource "aws_ssm_parameter" "cw_agent" {
+resource "aws_ssm_parameter" "cloudwatch_agent" {
   description = "Cloudwatch agent config to configure custom log"
   name        = "/cloudwatch-agent/config"
   type        = "String"
-  value       = file("cw_agent_config.json")
+  value       = file(var.cw)
 }
